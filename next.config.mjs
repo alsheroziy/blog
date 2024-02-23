6d5dab51589f05817eabea620fe8e1da2eb8a55f
Shehroz Raxmatov <raxmatshehroz1@gmail.com>
2024-02-23 21:11:57 +0500

    ozgartirildi

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+module.exports = {
+    output: {
+      // Bog'liq direktoriya nomi va sozlashlar
+      dir: 'out',
+      public: '/',
+    },
+  };
+  
